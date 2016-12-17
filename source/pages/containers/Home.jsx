20c8932ff0dcad46b9496d317fee71bb561768af
Miguh Ruiz <miguhruiz@gmail.com>
2016-12-17 18:56:57 +0100

    [Add] Infinite Scroll

--- source/pages/containers/Home.jsx
+++ source/pages/containers/Home.jsx
@@ -14,6 +14,8 @@ class Home extends Component {
             posts: [],
             loading: true
         }
+
+        this.handleScroll = this.handleScroll.bind(this)
     }
     async componentDidMount() {
         const posts = await api.posts.getList(this.state.page)
@@ -23,18 +25,55 @@ class Home extends Component {
             page: this.state.page + 1,
             loading: false
         })
+
+        window.addEventListener('scroll', this.handleScroll)
     }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll)
+    }
+
+    handleScroll(event) {
+        if(this.state.loading) return null
+
+        const scrolled = window.scrollY
+        const vHeight = window.innerHeight
+        const fullHeight = document.body.clientHeight
+
+        if(!(scrolled + vHeight + 300 >= fullHeight)) {
+            return null
+        }
+
+        this.setState({
+            loading: true
+        }, async () => {
+            try {
+                const posts = await api.posts.getList(this.state.page)
+                this.setState({
+                    posts: this.state.posts.concat(posts),
+                    page: this.state.page + 1,
+                    loading: false
+                })
+            } catch(error) {
+                console.error(error)
+                this.state({
+                    loading: false
+                })
+            }
+        })
+    }
+
     render() {
         return (
            <section name="Home">
                     <h1>Home</h1>
 
                     <section>
-                        {this.state.loading && (
-                            <Loading />
-                        )}
                         {this.state.posts.map(post =>
                             <Post key={post.id} {...post}/>
+                        )}
+                        {this.state.loading && (
+                            <Loading />
                         )}
                     </section>
                 </section>
